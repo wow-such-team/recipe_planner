fa9357aaee5976c2d0ecccc4e4f59be234092a3c
htsiang <htsiang3@gmail.com>
2019-04-22 21:16:23 -0700

    Added a few simple questions to quiz question array.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,9 @@
 
-var quizQuestions = [{qNum: 1, question: "", option1: "", option2: "", option3: "", option4: "", backImg: ""},
-                    {qNum: 2, question: "", option1: "", option2: "", option3: "", option4: "", backImg: ""},
-                    {qNum: 3, question: "", option1: "", option2: "", option3: "", option4: "", backImg: ""}];
+var quizQuestions = [{qNum: 1, question: "Pick your protein!", option1: "Beef", option2: "Chicken", option3: "Fish", option4: "Vegetarian", backImg: ""},
+                    {qNum: 2, question: "Spice Level?", option1: "Not spicy at all", option2: "Medium Spice", option3: "Spicy", option4: "Extra spicy", backImg: ""},
+                    {qNum: 3, question: "How many are we cooking for?", option1: "", option2: "", option3: "", option4: "", backImg: ""},
+                    {qNum: 4, question: "How much time do you have?", option1: "", backImg: ""}];
 
 $(document).ready(function() {
 
 });
-
-var card = $("#quiz-area");
-
-// Question set
-var questions = [{
-  question: "What are we making?",
-  answers: ["Breakfast", "Lunch", "Dinner", "Side"],
-  
-}, {
-  question: "Pick your protein!",
-  answers: ["Beef", "Chicken", "Fish", "Vegetarian"],
-
-}, {
-  question: "How spicy?",
-  answers: ["Not spicy at all", "Medium spice", "Spicy", "Extra spicy"],
- 
-}, {
-  question: "How many are we cooking for?",
-  answers: [" "],
-  
-}, {
-  question: "How much time do you have?",
-  answers: [" "],
- 
-}];
