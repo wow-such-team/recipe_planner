b61f8ec0793a9fe7e050b891603f83b3ecdd6849
htsiang <htsiang3@gmail.com>
2019-04-22 20:41:45 -0700

    Test branch with changes to app.js.

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+var quizQuestions = [{qNum: 1, question: "", option1: "", option2: "", option3: "", option4: "", backImg: ""},
+                    {qNum: 2, question: "", option1: "", option2: "", option3: "", option4: "", backImg: ""},
+                    {qNum: 3, question: "", option1: "", option2: "", option3: "", option4: "", backImg: ""}];
+
+$(document).ready(function() {
+
+});
