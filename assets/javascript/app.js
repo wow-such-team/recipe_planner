89b03e76c56f4e117619f22a83e86b120bebf7f9
Kim Gorcyca <kgorcyca@gmail.com>
2019-04-22 20:18:03 -0700

    additional css files for all html files

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+var card = $("#quiz-area");
+
+// Question set
+var questions = [{
+  question: "What are we making?",
+  answers: ["Breakfast", "Lunch", "Dinner", "Side"],
+  
+}, {
+  question: "Pick your protein!",
+  answers: ["Beef", "Chicken", "Fish", "Vegetarian"],
+
+}, {
+  question: "How spicy?",
+  answers: ["Not spicy at all", "Medium spice", "Spicy", "Extra spicy"],
+ 
+}, {
+  question: "How many are we cooking for?",
+  answers: [" "],
+  
+}, {
+  question: "How much time do you have?",
+  answers: [" "],
+ 
+}];
